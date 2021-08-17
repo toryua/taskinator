b006f034a300eccb4d74ae07f4b3685f8da16025
toryua <87789457+toryua@users.noreply.github.com>
2021-08-17 12:50:03 -0700

    createTaskHandler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function () {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listitemEl);
-}
-buttonEl.addEventListener("click", createTaskHandler);
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
 
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
+
+  formEl.addEventListener("submit", createTaskHandler);
 
