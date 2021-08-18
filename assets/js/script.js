18367bf723e9ff3e5ed4c00e04b2db68baf5295b
toryua <87789457+toryua@users.noreply.github.com>
2021-08-18 14:01:10 -0700

    added form capabilities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,28 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIDCounter = 0;
+var createTaskEl = function(taskDataObj)
+ {
+     var listItemEl = document.createElement("li");
+     listItemEl.className = "task-item";
 
-var createTaskHandler = function () {
+     // add task id as a custom attribute
+     listItemEl.setAttribute("data-task-id", taskIdCounter);
+
+     var taskInfoEl = document.createElement("div");
+     taskInfoEl.className = "task-info";
+     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name+ "</h3><span class='task=type'>"  + taskDataObj.type + "</span>";
+     listItemEl.appendChild(taskInfoEl);
+
+     tasksToDoEl.appendChild(listItemEl);
+
+     //increase task counter for next unique id
+     taskIdCounter++;
+ }
+var createTaskHandler = function (event) {
+
+    event.preventDefault ();
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
